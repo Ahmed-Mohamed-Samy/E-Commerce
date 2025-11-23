1eee6e70faf683be044fc11a58100d5c977df18f
Ahmed-Mohamed-Samy <AhmedSamy1ami@gmail.com>
2025-11-23 22:19:44 +0200

    Add Exception Handler

---
+++ ApplicationCoreLayer/E-Commerce.Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Services.Exceptions
+{
+    public abstract class NotFoundException(string Msg) : Exception(Msg)
+    {
+    }
+}
---
+++ PresentationLayer/E-Commerce.Web/Factories/ApiResponseFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Web.Factories
+{
+    public static class ApiResponseFactory
+    {
+        public static IActionResult GenerateApiValidationResponse(ActionContext actionContext)
+        {
+            var Errors = actionContext.ModelState.Where(E => E.Value.Errors.Count > 0)
+                                                   .ToDictionary(X => X.Key, X => X.Value.Errors.Select(X => X.ErrorMessage).ToList());
+            var Problem = new ProblemDetails()
+            {
+                Title = "Validation Errors",
+                Detail = "One or more Validation errors occurred",
+                Status = StatusCodes.Status400BadRequest,
+                Extensions =
+                        {
+                            {
+                                "Errors", Errors
+                            }
+                        }
+            };
+            return new BadRequestObjectResult(Problem);
+        }
+    }
+}
